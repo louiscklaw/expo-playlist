ebd969bc732aa7b0f90d8edf8bfc29ce7e189ee9
louiscklaw <louiscklaw@gmail.com>
2021-04-11 00:51:47 +0800

    update adding camping-app,

---
+++ camping-app/build.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+set -ex
+
+yarn
+# expo update 37
+# https://docs.expo.io/workflow/upgrading-expo-sdk-walkthrough/
+# yarn upgrade expo
+# yarn upgrade react
+# yarn upgrade react-native
+# yarn upgrade react-native-web
+
+yarn start
