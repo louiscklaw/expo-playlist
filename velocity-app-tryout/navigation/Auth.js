48efa6f26143798e64fa6ce0d421a17f814298cb
louiscklaw <louiscklaw@gmail.com>
2021-04-11 01:11:32 +0800

    update testing android app,

---
+++ velocity-app-tryout/navigation/Auth.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { createStackNavigator } from "react-navigation-stack";
+
+import Login from "../screens/Login";
+import Register from "../screens/Register";
+import Forgot from "../screens/Forgot";
+
+export default createStackNavigator(
+  {
+    Login,
+    Register,
+    Forgot
+  },
+  {
+    defaultNavigationOptions: {
+      header: null
+    }
+  }
+);
---
+++ velocity-app-tryout/navigation/Overview.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { createStackNavigator } from "react-navigation-stack";
+
+import Overview from "../screens/Overview";
+
+export default createStackNavigator({
+  Overview
+});
