0741f61a79024093aa88fdb19960872b815f027b
louiscklaw <louiscklaw@gmail.com>
2021-10-17 01:32:49 +0800

    update,

--- RNReactNavigation/ThemesTryout/BasicUsageTryout/init.sh
+++ RNReactNavigation/ThemesTryout/BasicUsageTryout/init.sh
@@ -3,17 +3,17 @@
 set -ex
 
 
-# rm -rf node_modules
+rm -rf node_modules
 rm -rf /tmp/metro-*
 
 yarn --dev
 
 yarn add @react-navigation/bottom-tabs
-yarn add @react-navigation/drawer
-yarn add @react-navigation/stack
-yarn add react-native-appearance
-yarn add react-native-gesture-handler
-yarn add react-native-reanimated
+# yarn add @react-navigation/drawer
+# yarn add @react-navigation/stack
+# yarn add react-native-appearance
+# yarn add react-native-gesture-handler
+# yarn add react-native-reanimated
 
 yarn android
 
