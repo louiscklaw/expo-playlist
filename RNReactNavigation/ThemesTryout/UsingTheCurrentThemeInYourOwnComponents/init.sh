15dc50890e630afc2ee82591d5a7d94b7cb6d203
louiscklaw <louiscklaw@gmail.com>
2021-10-17 01:38:57 +0800

    update,

---
+++ RNReactNavigation/ThemesTryout/UsingTheCurrentThemeInYourOwnComponents/init.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env zsh
+
+set -ex
+
+
+rm -rf node_modules
+rm -rf /tmp/metro-*
+
+yarn --dev
+
+yarn add @react-navigation/bottom-tabs
+yarn add @react-navigation/drawer
+yarn add @react-navigation/stack
+yarn add react-native-appearance
+yarn add react-native-gesture-handler
+yarn add react-native-reanimated
+
+yarn android
+
+yarn start --resetCache
