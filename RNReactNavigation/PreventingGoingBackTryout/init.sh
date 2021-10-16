e2d218b2fe68c37f993aa98e44985c0b4a343c20
louiscklaw <louiscklaw@gmail.com>
2021-10-16 22:16:16 +0800

    update,

---
+++ RNReactNavigation/PreventingGoingBackTryout/init.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env zsh
+
+set -ex
+
+
+yarn add @react-navigation/bottom-tabs
+yarn add @react-navigation/stack
+yarn add react-native-gesture-handler
+yarn add react-native-paper
+
+yarn android
