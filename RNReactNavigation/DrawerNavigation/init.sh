a768819e94443965256801fd58c9474e87afa107
louiscklaw <louiscklaw@gmail.com>
2021-10-16 21:12:13 +0800

    update,

---
+++ RNReactNavigation/DrawerNavigation/init.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env zsh
+
+set -ex
+
+
+yarn add @react-navigation/bottom-tabs
+yarn add @react-navigation/drawer
+yarn add react-native-gesture-handler react-native-reanimated
+
+yarn android
