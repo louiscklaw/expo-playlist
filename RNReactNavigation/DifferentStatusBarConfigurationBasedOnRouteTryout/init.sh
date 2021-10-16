c68cc243bda744530ae9ba2fe5360402d929d95e
louiscklaw <louiscklaw@gmail.com>
2021-10-16 21:55:50 +0800

    update,

---
+++ RNReactNavigation/DifferentStatusBarConfigurationBasedOnRouteTryout/init.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env zsh
+
+set -ex
+
+
+yarn add @react-navigation/bottom-tabs
+yarn add @react-navigation/stack
+yarn add react-native-gesture-handler
+yarn add react-native-safe-area-view
+
+yarn android
