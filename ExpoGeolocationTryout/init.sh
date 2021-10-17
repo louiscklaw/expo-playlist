4dc4920b4a603802edac161b6f9d6f20ff70c617
louiscklaw <louiscklaw@gmail.com>
2021-10-17 18:52:14 +0800

    update,

---
+++ ExpoGeolocationTryout/init.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+set -ex
+
+# yarn add react-navigation
+# yarn add react-native-animatable
+# yarn add react-navigation-fluid-transitions
+yarn add react-native-gesture-handler
+# yarn add react-navigation-stack
+# yarn add @react-navigation/native
+# yarn add @react-navigation/stack
+# yarn add expo-location
+
+# yarn --dev
+# yarn android
