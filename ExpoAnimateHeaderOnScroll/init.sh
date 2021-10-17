3debe7c9f87704a14669a88f4f5b3e0e1222e719
louiscklaw <louiscklaw@gmail.com>
2021-10-17 18:57:10 +0800

    update,

---
+++ ExpoAnimateHeaderOnScroll/init.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+set -ex
+
+# yarn add react-navigation
+# yarn add react-native-animatable
+# yarn add react-navigation-fluid-transitions
+# yarn add react-native-gesture-handler
+# yarn add react-navigation-stack
+yarn add react-native-safe-area-view
