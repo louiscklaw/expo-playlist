c13e60eb0174cc9ac6eb04a9382ab48610c41d6c
louiscklaw <louiscklaw@gmail.com>
2021-10-17 20:13:43 +0800

    update,

---
+++ ExpoOnBoardingViewPager/init.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+set -ex
+
+# yarn add @react-navigation/native
+# yarn add @react-navigation/stack
+# yarn add @react-native-community/viewpager
+yarn add react-native-gesture-handler
+
+yarn android -c
