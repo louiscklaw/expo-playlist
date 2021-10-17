78c5c527c4ccd2e59232dd46722c22aec10de5cb
louiscklaw <louiscklaw@gmail.com>
2021-10-17 18:45:10 +0800

    update,

---
+++ ExpoFormikTryout/init.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+set -ex
+
+# yarn add @react-navigation/native
+# yarn add @react-navigation/stack
+# yarn add react-native-gesture-handler
+
+# yarn add formik
+# yarn add yup
+
+yarn --dev
+yarn android
