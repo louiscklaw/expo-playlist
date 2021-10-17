1b9926764289a07fac831a5af1b8eaeb29eb7877
louiscklaw <louiscklaw@gmail.com>
2021-10-17 18:30:43 +0800

    update,

---
+++ ExpoReactNativeSharedElementTransitionTryout/config/data.js
@@ -0,0 +1,104 @@
+// Images in this example demo are being user from Unsplash
+// Manarola - https://unsplash.com/photos/rknrvCrfS1k
+// Venezia - https://unsplash.com/photos/hFXZ5cNfkOk
+// Prague - https://unsplash.com/photos/pz0P5piDQXs
+
+export const data = [
+  {
+    id: "1",
+    title: "Manarola, Italy",
+    description: "The Cliffs of Cinque Terre",
+    image_url:
+      "https://cdn.jsdelivr.net/gh/louiscklaw/test-resources@master/1200px-Dominos_pizza_logo.svg.png",
+    iconName: "location-pin",
+  },
+
+  {
+    id: "2",
+    title: "Venezia, Italy",
+    description: "Rialto Bridge, Venezia, Italy",
+    image_url:
+      "https://cdn.jsdelivr.net/gh/louiscklaw/test-resources@master/172533351_4259330820766603_9110805555440732879_n.jpg",
+    iconName: "location-pin",
+  },
+  {
+    id: "3",
+    title: "Prague, Czechia",
+    description: "Tram in Prague",
+    image_url:
+      "https://cdn.jsdelivr.net/gh/louiscklaw/test-resources@master/28577575_1584158765038839_1798510628361803062_n.jpg",
+    iconName: "location-pin",
+  },
+  {
+    id: "4",
+    title: "Prague, Czechia",
+    description: "Tram in Prague",
+    image_url:
+      "https://cdn.jsdelivr.net/gh/louiscklaw/test-resources@master/28577575_1584158765038839_1798510628361803062_n.jpg",
+    iconName: "location-pin",
+  },
+  {
+    id: "5",
+    title: "Prague, Czechia",
+    description: "Tram in Prague",
+    image_url:
+      "https://cdn.jsdelivr.net/gh/louiscklaw/test-resources@master/28577575_1584158765038839_1798510628361803062_n.jpg",
+    iconName: "location-pin",
+  },
+  {
+    id: "6",
+    title: "Prague, Czechia",
+    description: "Tram in Prague",
+    image_url:
+      "https://cdn.jsdelivr.net/gh/louiscklaw/test-resources@master/28577575_1584158765038839_1798510628361803062_n.jpg",
+    iconName: "location-pin",
+  },
+  {
+    id: "7",
+    title: "Prague, Czechia",
+    description: "Tram in Prague",
+    image_url:
+      "https://cdn.jsdelivr.net/gh/louiscklaw/test-resources@master/28577575_1584158765038839_1798510628361803062_n.jpg",
+    iconName: "location-pin",
+  },
+  {
+    id: "8",
+    title: "Prague, Czechia",
+    description: "Tram in Prague",
+    image_url:
+      "https://cdn.jsdelivr.net/gh/louiscklaw/test-resources@master/28577575_1584158765038839_1798510628361803062_n.jpg",
+    iconName: "location-pin",
+  },
+  {
+    id: "9",
+    title: "Prague, Czechia",
+    description: "Tram in Prague",
+    image_url:
+      "https://cdn.jsdelivr.net/gh/louiscklaw/test-resources@master/28577575_1584158765038839_1798510628361803062_n.jpg",
+    iconName: "location-pin",
+  },
+  {
+    id: "10",
+    title: "Prague, Czechia",
+    description: "Tram in Prague",
+    image_url:
+      "https://cdn.jsdelivr.net/gh/louiscklaw/test-resources@master/28577575_1584158765038839_1798510628361803062_n.jpg",
+    iconName: "location-pin",
+  },
+  {
+    id: "11",
+    title: "Prague, Czechia",
+    description: "Tram in Prague",
+    image_url:
+      "https://cdn.jsdelivr.net/gh/louiscklaw/test-resources@master/28577575_1584158765038839_1798510628361803062_n.jpg",
+    iconName: "location-pin",
+  },
+  {
+    id: "12",
+    title: "Prague, Czechia",
+    description: "Tram in Prague",
+    image_url:
+      "https://cdn.jsdelivr.net/gh/louiscklaw/test-resources@master/28577575_1584158765038839_1798510628361803062_n.jpg",
+    iconName: "location-pin",
+  },
+];
---
+++ ExpoReactNativeSharedElementTransitionTryout/init.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env zsh
+
+set -ex
+
+
+# yarn --dev
+# yarn add @react-navigation/native
+# yarn add @react-navigation/stack
+# yarn add react-native-shared-element
+yarn add react-native-gesture-handler
+# yarn add react-navigation-shared-element
+# yarn add react-native-animatable
+
+# yarn android
+
+# yarn start
