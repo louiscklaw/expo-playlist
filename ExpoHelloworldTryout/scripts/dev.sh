d69e03a7f585a0867283338615b743da418ed59e
louiscklaw <louiscklaw@gmail.com>
2021-10-23 19:46:20 +0800

    update,

--- ExpoHelloworldTryout/scripts/dev.sh
+++ ExpoHelloworldTryout/scripts/dev.sh
@@ -2,17 +2,17 @@
 
 set -ex
 
-sudo yarn global remove expo-cli || true
-sudo yarn global add expo-cli
-
+rm -rf node_modules || true
 rm -rf /tmp/metro-* || true
 
-# yarn add react-navigation
-# yarn add react-native-animatable
-# yarn add react-navigation-fluid-transitions
-# yarn add react-native-gesture-handler
-# yarn add react-navigation-stack
-# yarn add react-native-screens
-
 yarn --dev
-yarn android -c
+
+# yarn add expo
+# yarn add react
+# yarn add react-dom
+# yarn add react-native
+# yarn add react-native-web
+
+expo install expo-status-bar
+
+yarn android
