35996f0e97cfd5794184a07194517b47a4f54a1c
louiscklaw <louiscklaw@gmail.com>
2021-10-24 01:06:32 +0800

    update,

---
+++ ExpoSDKTryout/ImageManipulatorTryout/App.js
@@ -0,0 +1,60 @@
+import React, { useState, useEffect } from "react";
+import { Button, Image, StyleSheet, View } from "react-native";
+import { Asset } from "expo-asset";
+import { manipulateAsync, FlipType, SaveFormat } from "expo-image-manipulator";
+
+export default function App() {
+  const [ready, setReady] = useState(false);
+  const [image, setImage] = useState(null);
+
+  useEffect(() => {
+    (async () => {
+      const image = Asset.fromModule(require("./assets/test.jpg"));
+      await image.downloadAsync();
+      setImage(image);
+      setReady(true);
+    })();
+  }, []);
+
+  const _rotate90andFlip = async () => {
+    const manipResult = await manipulateAsync(
+      image.localUri || image.uri,
+      [{ rotate: 90 }, { flip: FlipType.Vertical }],
+      { compress: 1, format: SaveFormat.PNG }
+    );
+    setImage(manipResult);
+  };
+
+  const _renderImage = () => (
+    <View style={styles.imageContainer}>
+      <Image
+        source={{ uri: image.localUri || image.uri }}
+        style={styles.image}
+      />
+    </View>
+  );
+
+  return (
+    <View style={styles.container}>
+      {ready && image && _renderImage()}
+      <Button title="Rotate and Flip" onPress={_rotate90andFlip} />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+  },
+  imageContainer: {
+    marginVertical: 20,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  image: {
+    width: 300,
+    height: 300,
+    resizeMode: "contain",
+  },
+});
