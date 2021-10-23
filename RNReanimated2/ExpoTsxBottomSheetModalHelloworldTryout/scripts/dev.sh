cd1edd341ca250004bbf762e8949a8ac0b0a2198
louiscklaw <louiscklaw@gmail.com>
2021-10-23 19:28:52 +0800

    update,

--- RNReanimated2/ExpoTsxBottomSheetModalHelloworldTryout/scripts/dev.sh
+++ RNReanimated2/ExpoTsxBottomSheetModalHelloworldTryout/scripts/dev.sh
@@ -2,8 +2,13 @@
 
 set -ex
 
+rm -rf node_modules || true
+rm -rf /tmp/metro-* || true
+
+yarn add react-native-reanimated
+yarn add @gorhom/bottom-sheet@^4
+yarn add react-native-gesture-handler
+
 yarn --dev
 
 yarn android
-
-expo start --clear
--- RNReanimated2/ExpoTsxBottomSheetModalHelloworldTryout/scripts/init.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-set -ex
-
-expo install react-native-reanimated
-expo install @gorhom/bottom-sheet@^4
-expo install react-native-gesture-handler
