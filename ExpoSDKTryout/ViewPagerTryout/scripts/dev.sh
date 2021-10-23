50f03c1367d773bb701c119d3644f71d84100705
louiscklaw <louiscklaw@gmail.com>
2021-10-24 01:57:50 +0800

    update,

---
+++ ExpoSDKTryout/ViewPagerTryout/scripts/dev.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+set -ex
+
+# rm -rf node_modules || true
+rm -rf /tmp/metro-* || true
+
+yarn --dev
+
+# yarn add expo
+# yarn add react
+# yarn add react-dom
+# yarn add react-native
+# yarn add react-native-web
+
+expo install expo-status-bar
+expo install react-native-pager-view
+
+# yarn android
