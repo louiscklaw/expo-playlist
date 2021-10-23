941b8201bf248522c53be8727f5e23fea9f812bf
louiscklaw <louiscklaw@gmail.com>
2021-10-24 01:27:29 +0800

    update,

---
+++ ExpoSDKTryout/LottieTryout/scripts/dev.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+set -ex
+
+# rm -rf node_modules || true
+rm -rf /tmp/metro-* || true
+
+yarn --dev
+
+# yarn add expo
+# yarn add react
+# yarn add react-dom
+# yarn add react-native
+# yarn add react-native-web
+
+expo install expo-status-bar
+expo install lottie-react-native
+
+# yarn android
