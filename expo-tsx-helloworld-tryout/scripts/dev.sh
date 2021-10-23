3cc38e8e8d00f148c75bf253a5f31e868ded1eb4
louiscklaw <louiscklaw@gmail.com>
2021-10-23 14:48:53 +0800

    update,

---
+++ expo-tsx-helloworld-tryout/scripts/dev.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+set -ex
+
+rm -rf node_modules || true
+rm -rf /tmp/metro-* || true
+
+# yarn add expo
+# yarn add react react-dom
+# yarn add react-native react-native-web
+
+# yarn add --dev @babel/core @types/react @types/react-native typescript
+
+# sudo yarn global remove expo-cli || true
+# sudo yarn global add expo-cli
+
+# yarn add react-navigation
+# yarn add react-native-animatable
+# yarn add react-navigation-fluid-transitions
+# yarn add react-native-gesture-handler
+# yarn add react-navigation-stack
+# yarn add react-native-screens
+
+yarn --dev
+
+yarn start -c
