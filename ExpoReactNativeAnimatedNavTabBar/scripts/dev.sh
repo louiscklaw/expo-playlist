3cb5a0f3079b85dd8a20c039906cd3f58b2d358f
louiscklaw <louiscklaw@gmail.com>
2021-10-23 19:40:10 +0800

    update,

--- ExpoReactNativeAnimatedNavTabBar/scripts/dev.sh
+++ ExpoReactNativeAnimatedNavTabBar/scripts/dev.sh
@@ -2,12 +2,9 @@
 
 set -ex
 
-# yarn add react-navigation
-# yarn add react-native-animatable
-# yarn add react-navigation-fluid-transitions
-# yarn add react-native-gesture-handler
-# yarn add react-navigation-stack
-# yarn add react-native-screens
+rm -rf node_modules || true
+rm -rf /tmp/metro-* || true
 
 yarn --dev
-yarn android -c
+
+yarn android
