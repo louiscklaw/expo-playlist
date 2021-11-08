f8ad0a82a260bdeecd00536a28730800116141a5
louiscklaw <louiscklaw@gmail.com>
2021-11-09 02:05:05 +0800

    update,

---
+++ ExpoRnWaveBottomBarTryout/App/ListScreen.tsx
@@ -0,0 +1,68 @@
+import React from 'react';
+import {ScrollView, Text, View} from 'react-native';
+import {Avatar, Button, Card} from 'react-native-paper';
+import {SafeAreaView, useSafeArea} from 'react-native-safe-area-context';
+
+const LeftContent = (props: any) => <Avatar.Icon {...props} icon="folder" />;
+
+export function SettingsScreen() {
+  const {top} = useSafeArea();
+  return (
+    <View style={{flex: 1, marginTop: top}}>
+      <ScrollView>
+        <View>
+          <Card elevation={5} style={{margin: 5}}>
+            <Card.Title
+              title="Card Title"
+              subtitle="Card Subtitle"
+              left={LeftContent}
+            />
+            <Card.Cover source={{uri: 'https://picsum.photos/700'}} />
+            <Card.Actions>
+              <Button>Cancel</Button>
+              <Button>Ok</Button>
+            </Card.Actions>
+          </Card>
+          <Card elevation={5} style={{margin: 5}}>
+            <Card.Title
+              title="Card Title"
+              subtitle="Card Subtitle"
+              left={LeftContent}
+            />
+            <Card.Cover source={{uri: 'https://picsum.photos/700'}} />
+            <Card.Actions>
+              <Button>Cancel</Button>
+              <Button>Ok</Button>
+            </Card.Actions>
+          </Card>
+          <Card elevation={5} style={{margin: 5}}>
+            <Card.Title
+              title="Card Title"
+              subtitle="Card Subtitle"
+              left={LeftContent}
+            />
+            <Card.Cover source={{uri: 'https://picsum.photos/700'}} />
+            <Card.Actions>
+              <Button>Cancel</Button>
+              <Button>Ok</Button>
+            </Card.Actions>
+          </Card>
+          <Card elevation={5} style={{margin: 5}}>
+            <Card.Title
+              title="Card Title"
+              subtitle="Card Subtitle"
+              left={LeftContent}
+            />
+            <Card.Cover source={{uri: 'https://picsum.photos/700'}} />
+            <Card.Actions>
+              <Button>Cancel</Button>
+              <Button>Ok</Button>
+            </Card.Actions>
+          </Card>
+        </View>
+        {/* -> Set bottom view to allow scrolling to top if you set bottom-bar position absolute */}
+        <View style={{height: 90}} />
+      </ScrollView>
+    </View>
+  );
+}
---
+++ ExpoRnWaveBottomBarTryout/scripts/init.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+set -ex
+
+# expo install react-native-paper
+# expo install @react-navigation/bottom-tabs
+# expo install @react-navigation/native
+# expo install rn-wave-bottom-bar
+expo install react-native-svg
