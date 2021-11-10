7c5009ea265280f7029634497036ac9b188081ce
louiscklaw <louiscklaw@gmail.com>
2021-11-10 13:55:42 +0800

    update,

--- ExpoReactNativeSharedElementTransitionTryout/scripts/dev.sh
+++ ExpoReactNativeSharedElementTransitionTryout/scripts/dev.sh
@@ -5,11 +5,10 @@ set -ex
 # rm -rf node_modules || true
 # rm -rf /tmp/metro-* || true
 
-# adb shell am force-stop host.exp.exponent || true
-# sleep 5
-
-# adb shell pm uninstall host.exp.exponent || true
-# sleep 5
+adb shell am force-stop host.exp.exponent || true
+sleep 3
+adb shell pm uninstall host.exp.exponent || true
+sleep 3
 
 expo install react-native-shared-element
 
