6dd46bc5583a78f600207eb9686829056dcb959b
Louis Law <louiscklaw@users.noreply.github.com>
2022-04-11 16:28:22 +0800

    update ExpoHelloworld,

---
+++ _design_system/now-ui-react-native-helloworld/scripts/uninstall_expo.ps1
@@ -0,0 +1,8 @@
+adb shell am force-stop host.exp.exponent
+adb shell pm uninstall host.exp.exponent
+
+adb shell am force-stop host.exp.exponent
+adb shell pm uninstall host.exp.exponent
+
+adb shell am force-stop host.exp.exponent
+adb shell pm uninstall host.exp.exponent
