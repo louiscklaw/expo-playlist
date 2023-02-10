fe940a4118d297a266d56990a9e630123bfbe660
louiscklaw <louiscklaw@users.noreply.github.com>
2023-02-10 20:34:47 +0800

    update working,

--- Ignite/IgniteExpoCliHelloworldRNEUITryout/app/screens/LoginScreen.tsx
+++ Ignite/IgniteExpoCliHelloworldRNEUITryout/app/screens/LoginScreen.tsx
@@ -6,6 +6,12 @@ import { useStores } from "../models"
 import { AppStackScreenProps } from "../navigators"
 import { colors, spacing } from "../theme"
 
+import { Button as RNEButton } from "@rneui/themed"
+import { ButtonGroup } from "@rneui/themed"
+import { Card, Text as RNEText } from "@rneui/themed"
+import { Chip, withTheme, lightColors } from "@rneui/themed"
+import { Icon as RNEIcon } from '@rneui/themed';
+
 interface LoginScreenProps extends AppStackScreenProps<"Login"> {}
 
 export const LoginScreen: FC<LoginScreenProps> = observer(function LoginScreen(_props) {
@@ -64,6 +70,9 @@ export const LoginScreen: FC<LoginScreenProps> = observer(function LoginScreen(_
     }
   }, [])
 
+  const [selectedIndex, setSelectedIndex] = useState(0)
+  const [selectedIndexes, setSelectedIndexes] = useState([0, 2, 3])
+
   return (
     <Screen preset="auto" contentContainerStyle={$screenContentContainer} safeAreaEdges={["top", "bottom"]}>
       <Text testID="login-heading" tx="loginScreen.signIn" preset="heading" style={$signIn} />
@@ -101,6 +110,90 @@ export const LoginScreen: FC<LoginScreenProps> = observer(function LoginScreen(_
         onSubmitEditing={login}
         RightAccessory={PasswordRightAccessory}
       />
+
+
+<Card>
+  <RNEText>Word of the Day</RNEText>
+  <RNEText h4>be-nev-o=lent</RNEText>
+  <RNEText>adjective</RNEText>
+  <RNEText>
+    well meaning and kindly.
+    {'"a benevolent smile"'}
+  </RNEText>
+  <RNEButton size="sm" type="clear">
+    Learn More
+  </RNEButton>
+</Card>
+
+<Chip title="Solid Chip" containerStyle={{ marginVertical: 15 }} />
+<Chip title="Outlined Chip" type="outline" containerStyle={{ marginVertical: 15 }} />
+<Chip
+  title="Outlined & Disabled"
+  type="outline"
+  disabled
+  containerStyle={{ marginVertical: 15 }}
+/>
+
+<Chip
+  title="Left Icon Chip"
+  icon={{
+    name: "bluetooth",
+    type: "font-awesome",
+    size: 20,
+    color: "white",
+  }}
+  containerStyle={{ marginVertical: 15 }}
+/>
+
+<Chip
+  title="Right Icon Chip"
+  icon={{
+    name: "close",
+    type: "font-awesome",
+    size: 20,
+    color: "white",
+  }}
+  iconRight
+  containerStyle={{ marginVertical: 15 }}
+/>
+
+<Chip
+  title="Pressable Icon Chip"
+  icon={{
+    name: "bluetooth",
+    type: "font-awesome",
+    size: 20,
+    color: lightColors.primary2,
+  }}
+  onPress={() => console.log("Icon chip was pressed!")}
+  type="outline"
+  containerStyle={{ marginVertical: 15 }}
+/>
+
+<Chip
+  title="Pressable Icon Chip"
+  icon={{
+    name: "close",
+    type: "font-awesome",
+    size: 20,
+    color: lightColors.primary2,
+  }}
+  onPress={() => console.log("Icon chip was pressed!")}
+  iconRight
+  type="outline"
+  containerStyle={{ marginVertical: 15 }}
+/>
+
+<ButtonGroup
+        buttons={["SIMPLE", "BUTTON", "GROUP"]}
+        selectedIndex={selectedIndex}
+        onPress={(value) => {
+          setSelectedIndex(value)
+        }}
+        containerStyle={{ marginBottom: 20 }}
+      />
+
+      <RNEButton title="Solid" />
 
       <Button testID="login-button" tx="loginScreen.tapToSignIn" style={$tapButton} preset="reversed" onPress={login} />
     </Screen>
