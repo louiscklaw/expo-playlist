7a73c5941be0a7f2a86621dc388d73bfa477a9d7
louiscklaw <louiscklaw@users.noreply.github.com>
2023-02-10 14:13:18 +0800

    update,

--- IgniteExpoCliHelloworldRNEUITryout/app/screens/LoginScreen.tsx
+++ IgniteExpoCliHelloworldRNEUITryout/app/screens/LoginScreen.tsx
@@ -9,6 +9,7 @@ import { colors, spacing } from "../theme"
 import { Button as RNEButton } from "@rneui/themed"
 import { ButtonGroup } from "@rneui/themed"
 import { Card, Text as RNEText } from "@rneui/themed"
+import { Chip, withTheme, lightColors } from "@rneui/themed"
 
 interface LoginScreenProps extends AppStackScreenProps<"Login"> {}
 
@@ -132,6 +133,62 @@ export const LoginScreen: FC<LoginScreenProps> = observer(function LoginScreen(_
         <RNEButton size="sm" type="clear">
           Learn More
         </RNEButton>
+
+        <Chip title="Solid Chip" containerStyle={{ marginVertical: 15 }} />
+        <Chip title="Disabled Chip" disabled containerStyle={{ marginVertical: 15 }} />
+        <Chip title="Outlined Chip" type="outline" containerStyle={{ marginVertical: 15 }} />
+        <Chip
+          title="Outlined & Disabled"
+          type="outline"
+          disabled
+          containerStyle={{ marginVertical: 15 }}
+        />
+        <Chip
+          title="Left Icon Chip"
+          icon={{
+            name: "bluetooth",
+            type: "font-awesome",
+            size: 20,
+            color: "white",
+          }}
+          containerStyle={{ marginVertical: 15 }}
+        />
+        <Chip
+          title="Right Icon Chip"
+          icon={{
+            name: "close",
+            type: "font-awesome",
+            size: 20,
+            color: "white",
+          }}
+          iconRight
+          containerStyle={{ marginVertical: 15 }}
+        />
+        <Chip
+          title="Pressable Icon Chip"
+          icon={{
+            name: "bluetooth",
+            type: "font-awesome",
+            size: 20,
+            color: lightColors.primary2,
+          }}
+          onPress={() => console.log("Icon chip was pressed!")}
+          type="outline"
+          containerStyle={{ marginVertical: 15 }}
+        />
+        <Chip
+          title="Pressable Icon Chip"
+          icon={{
+            name: "close",
+            type: "font-awesome",
+            size: 20,
+            color: lightColors.primary2,
+          }}
+          onPress={() => console.log("Icon chip was pressed!")}
+          iconRight
+          type="outline"
+          containerStyle={{ marginVertical: 15 }}
+        />
       </Card>
 
       <ButtonGroup
